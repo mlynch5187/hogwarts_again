590b2f21b9dbd6aa362fbec51d77f5fe188176ca
Alex Lynch <mlynch5187@gmail.com>
2020-04-01 10:51:29 -0600

    Complete user story 1

--- app/controllers/professor_controller.rb
+++ app/controllers/professor_controller.rb
@@ -3,10 +3,4 @@ class ProfessorController < ApplicationController
   def index
     @professors = Professor.all
   end
-
-  def show
-    teacher = Professor.find(params[:id])
-    @students = Student.where(professor: teacher)
-  end
-
 end
