0e6a547a508540303636c20d7c92f6f3afe520d1
Alex Lynch <mlynch5187@gmail.com>
2020-04-01 10:30:47 -0600

    Set up professor/student relationships

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student <ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :house
+
   has_many :professor_students
   has_many :professors, through: :professor_students
-
-  validates_presence_of :name, :age, :house
 end
